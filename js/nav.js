eb90466260457f0ecea484fe7ae5f2109f902631
skillnull <skillisnull@gmail.com>
2019-01-24 16:31:16 +0800

    add new animation

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+document.writeln("<a href=\"../index.html\">index</a>");
+document.writeln("<a href=\"demo1.html\">demo1</a>");
+document.writeln("<a href=\"demo2.html\">demo2</a>");
+document.writeln("<a href=\"demo3.html\">demo3</a>");
+document.writeln("<a href=\"demo4.html\">demo4</a>");
+document.writeln("<a href=\"demo5.html\">demo5</a>");
